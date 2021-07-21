47aa7404fc91fa1061ee419ad6018029c82f339b
wae <seshnnlqt@gmail.com>
2021-07-21 17:51:16 +0700

    lesson1

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+const text="Any text for message"
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name="GeekBrains" text={text} />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
